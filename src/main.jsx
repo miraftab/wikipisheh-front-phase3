87f1bb537bb60b54da4a70f57ec892f81ee5f8bc
Javad Miraftabzadeh <javad.miraftabzadeh@gmail.com>
2025-11-01 11:15:30 +0330

    main.jsx add ReactGA and initiate it

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ReactGA from "react-ga4";
+import { prefixer } from "stylis";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
-import { prefixer } from "stylis";
 import { default as rtlPlugin }  from "@mui/stylis-plugin-rtl";
 
 import { ThemeProvider } from "@mui/material/styles";
@@ -22,6 +23,8 @@ const cacheRtl = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
 });
+
+ReactGA.initialize("G-P3M8FFX55L");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
